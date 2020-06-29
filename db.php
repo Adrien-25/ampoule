3f31916ca8193f142f3694fe80a745749e37fe93
Adrien SCHMIDT <adrien.schmidt7@gmail.com>
2020-06-29 04:26:31 +0200

    Update - Modification de la connexion à la  base de donnée
    Connexion à la base de donnée sur l'hôte du serveur online

--- db.php
+++ db.php
@@ -1,10 +1,15 @@
 <?php
-define('DATABASE', 'gardien');
-define('USER', 'root');
-define('PWD', '');
-define('HOST','localhost');
+define('DATABASE', 'adriens_');
+//define('USER', 'root');
+define('USER', 'adriens');
+//define('PWD', '');
+define('PWD', 'sHXd4ZJ2qCL8rA==');
+//define('HOST','localhost');
+define('HOST','https://promo-39.codeur.online/adminer/?server=promo-39.codeur.online');
+//
+
 try {
-    $dbh = new PDO('mysql:host='.HOST.';dbname='.DATABASE, USER, PWD,array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+    $dbh = new PDO('mysql:host='.HOST.';dbname='.DATABASE, USER, PWD, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
 } catch (PDOException $e) {
     print "Erreur !: " . $e->getMessage() . "<br/>";
     die();
