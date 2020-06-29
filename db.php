9718c6fae5b9921ea1f34d90f15f69e69ae6aace
Adrien SCHMIDT <adrien.schmidt7@gmail.com>
2020-06-29 14:41:51 +0200

    Update - Modifcation du PDO
    Modification de la page de connexion à la base donnée pour se connecter avec Wamp

--- db.php
+++ db.php
@@ -1,12 +1,8 @@
 <?php
-define('DATABASE', 'adriens_');
-//define('USER', 'root');
-define('USER', 'adriens');
-//define('PWD', '');
-define('PWD', 'sHXd4ZJ2qCL8rA==');
-//define('HOST','localhost');
-define('HOST','https://promo-39.codeur.online/adminer/?server=promo-39.codeur.online');
-//
+define('DATABASE', 'gardien');
+define('USER', 'root');
+define('PWD', '');
+define('HOST','localhost');
 
 try {
     $dbh = new PDO('mysql:host='.HOST.';dbname='.DATABASE, USER, PWD, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
@@ -14,5 +10,4 @@ try {
     print "Erreur !: " . $e->getMessage() . "<br/>";
     die();
 }
-
 ?>
