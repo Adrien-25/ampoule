5019499af11db164b3389e3b51bfb94a65252d8e
Adrien SCHMIDT <adrien.schmidt7@gmail.com>
2020-06-29 15:55:27 +0200

    Add - Ajout vérification de login
    Ajout du test d'Authentification dans la page edit.php qui retourne a la page login.php si la session conteant le nom d'utilisateur est vide

--- edit.php
+++ edit.php
@@ -1,5 +1,10 @@
 <?php
+    session_start();
     require_once('db.php');
+    
+    if(empty($_SESSION['username'])){
+        header('Location: login.php');
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
