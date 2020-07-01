558f89e5c607b58c89015caf2fb6ef9473ab1415
Adrien SCHMIDT <adrien.schmidt7@gmail.com>
2020-07-01 17:26:25 +0200

    Update - Fintion fichier PDO

--- db.php
+++ db.php
@@ -2,7 +2,14 @@
 define('DATABASE', 'gardien');
 define('USER', 'root');
 define('PWD', '');
+
+/*define('DATABASE', 'adriens_ampoule');
+define('USER', 'adriens');
+define('PWD', 'sHXd4ZJ2qCL8rA==');
+*/
+
 define('HOST','localhost');
+
 
 try {
     $dbh = new PDO('mysql:host='.HOST.';dbname='.DATABASE, USER, PWD, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
